63f091186f35d77f1d368f6f4bf8fbfdb5bcec7a
Piotr Górski <piotrek.gorski90@gmail.com>
2018-07-11 11:15:19 +0200

    Add scripts for toggling receipt box and removing elements from list

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-var table = document.getElementById("table").addEventListener("click", alertFunction);
+function toggleReceiptBox(){
+    var table = document.getElementById("table").addEventListener("click", receiptBox);
+    var closeReceiptBox = document.getElementById("receipt-box").addEventListener("dblclick", receiptBox);
 
-function alertFunction(){
-    alert("CLICKING WORKS")
+    function receiptBox() {
+        var receiptBox = document.getElementById("receipt-box");
+        if(receiptBox.style.display === "none"){
+            receiptBox.style.display = "block";
+        }else {
+            receiptBox.style.display = "none";
+        }
+    }
+}
+
+function removeElementFromReceiptBox(){
+/* Get all elements with class="close" */
+    var closebtns = document.getElementsByClassName("close");
+    var i;
+
+/* Loop through the elements, and hide the parent, when clicked on */
+    for (i = 0; i < closebtns.length; i++) {
+      closebtns[i].addEventListener("click", function() {
+          this.parentElement.style.display = 'none';
+        });
+    }
 }
