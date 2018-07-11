d784c526e9fe1cf0d1d0e5c3562b20b5180f3e54
PiotrGry <piotrgryzlo@gmail.com>
2018-07-11 11:29:10 +0200

    Add widnow popup when icon is pressed

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 
-var table = document.getElementById("table").addEventListener("click", alertFunction);
+// Get the modal
+var modal = document.getElementById('myModal');
 
-function alertFunction(){
-    alert("CLICKING WORKS")
+// Get the button that opens the modal
+var btn = document.getElementById("table");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var add = document.getElementById("plus");
+add.onclick = function(){
+    alert("it works !");
 }
