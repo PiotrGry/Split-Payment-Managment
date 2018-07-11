3f2d418b0fcba6c90b543435a2c41d7ca34e8ebb
Piotr Górski <piotrek.gorski90@gmail.com>
2018-07-11 11:28:10 +0200

    Minor bugfix, removed functions to separatecode

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-function toggleReceiptBox(){
-    var table = document.getElementById("table").addEventListener("click", receiptBox);
-    var closeReceiptBox = document.getElementById("receipt-box").addEventListener("dblclick", receiptBox);
+var table = document.getElementById("table").addEventListener("click", receiptBox);
+var closeReceiptBox = document.getElementById("receipt-box").addEventListener("dblclick", receiptBox);
 
-    function receiptBox() {
-        var receiptBox = document.getElementById("receipt-box");
-        if(receiptBox.style.display === "none"){
-            receiptBox.style.display = "block";
-        }else {
-            receiptBox.style.display = "none";
-        }
+function receiptBox() {
+    var receiptBox = document.getElementById("receipt-box");
+    if(receiptBox.style.display === "none"){
+        receiptBox.style.display = "block";
+    }else {
+        receiptBox.style.display = "none";
     }
 }
 
-function removeElementFromReceiptBox(){
 /* Get all elements with class="close" */
-    var closebtns = document.getElementsByClassName("close");
-    var i;
+var closebtns = document.getElementsByClassName("close");
+var i;
 
 /* Loop through the elements, and hide the parent, when clicked on */
-    for (i = 0; i < closebtns.length; i++) {
-      closebtns[i].addEventListener("click", function() {
-          this.parentElement.style.display = 'none';
-        });
-    }
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+  this.parentElement.style.display = 'none';
+});
 }
