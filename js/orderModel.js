97246d8d507576129f51de2ddd7e2c60f48a7976
PiotrGry <piotrgryzlo@gmail.com>
2018-07-23 15:46:12 +0200

    Create order Model

---
+++ js/orderModel.js
@@ -0,0 +1,35 @@
+class OrderModel{
+    constructor(name, description, price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    setName(newName){
+        this.name= name;
+    }
+
+    getDescription(){
+        return this.description;
+    }
+    setDescription(newDescription){
+        this.description = newDescription;
+    }
+
+    getPrice(){
+        return this.price;
+    }
+
+    setPrice(newPrice){
+        this.price = newPrice;
+    }
+
+    
+
+
+
+}
